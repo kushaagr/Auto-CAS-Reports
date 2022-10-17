326a345cb690502c784d715b895e8f1770096acd
Kushagra Mehrotra <68564934+kushaagr@users.noreply.github.com>
2022-10-17 23:36:39 +0530

    Remove raise error statement

--- main.py
+++ main.py
@@ -160,7 +160,6 @@ def Perform_File_Operations(data_sheet: str, survey_id: int, survey_name: str):
     # codednames = rutil.generate_codenames_list(studata, pick_department.get())
     print(f'{codednames=}')
     rutil.Upload_Summary(reportsdir, tscdata, studata, codednames, allquestions , rawscores, survey_name)
-    raise ValueError
     rutil.Upload_All_Reports(reportsdir, tscdata, studata, graphs, survey_id, codednames)
     # rutil.Upload_All_Reports(reportsdir, tscdata, studata, graphs, survey_id, pick_department.get())
     # rootwindow.config(cursor='')
