1a506378ec5189495e159b94441966c1569a28a9
Kushagra Mehrotra <68564934+kushaagr@users.noreply.github.com>
2022-11-16 22:45:39 +0530

    Update file

--- package-code.bat
+++ package-code.bat
@@ -1,7 +1,9 @@
 @echo off
 ECHO Update AppVersion in .iss script
-TIMEOUT 3
+rem TIMEOUT 3
+@pause
 @echo on
+notepad3 "C:\git\Auto-CAS-Reports\create-setup.iss"
 pyinstaller -i ./images/icons8-feather-60.ico -w --noconfirm --add-data "./images/;images" --add-data "./myfonts/;myfonts" main.py
 iscc create-setup.iss
 
